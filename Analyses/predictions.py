8de7bfdafcbe95a7c0a6bf1b8c1c21a1843f3894
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2019-02-20 13:27:09 +0100

    code (plus) propre

--- Analyses/predictions.py
+++ Analyses/predictions.py
@@ -36,7 +36,7 @@ def get_orders(y,train_ratio):
 
     return aic_min_order,bic_min_order,mse_min_order
 
-def get_values(train_ratio,aic_min_order,bic_min_order,mse_min_order,y,plot = False)
+def get_values(train_ratio,aic_min_order,bic_min_order,mse_min_order,y,plot = False):
     aic_val = []
     bic_val=[]
     mse_val= []
@@ -52,7 +52,7 @@ def get_values(train_ratio,aic_min_order,bic_min_order,mse_min_order,y,plot = Fa
         
     return aic_val, bic_val, mse_val
 
-def get_predictions(train_ratio,y,aic_min_order,bic_min_order,mse_min_order):
+def get_predictions(train_ratio,y,aic_min_order,bic_min_order,mse_min_order,plot=False,signif =False):
     err_aic = []
     err_bic = []
     err_mse = []
@@ -61,22 +61,38 @@ def get_predictions(train_ratio,y,aic_min_order,bic_min_order,mse_min_order):
     
         print('MODEL AIC')
         (p,q) = aic_min_order[i]
-        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= False)
+        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= signif)
         err_aic.append(e)
     
         print('MODEL BIC')
         (p,q) = bic_min_order[i]
-        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= False)
+        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= signif)
         err_bic.append(e)
     
         print('MODEL MSE')
         (p,q) = mse_min_order[i]
-        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= False)
+        f,ci,e = out_of_sample_prediction(p=p, q=q, y_true=y, train_ratio=train_ratio[i],signif= signif)
         err_mse.append(e)
+     
+    if plot == True:
+        plt.bar([0,1,2],[err_aic[0],err_bic[0],err_mse[0]])
+        plt.xticks([0,1,2], ['aic_best_mod','bic_best_mod','mse_best_mod'])
+        plt.title('MSE pred, train_ratio = 0.25')
+        plt.show()
+
+        plt.bar([0,1,2],[err_aic[1],err_bic[1],err_mse[1]],color = 'g')
+        plt.xticks([0,1,2], ['aic_best_mod','bic_best_mod','mse_best_mod'])
+        plt.title('MSE pred, train_ratio = 0.5')
+        plt.show()
+
+        plt.bar([0,1,2],[err_aic[2],err_bic[2],err_mse[2]], color = 'r')
+        plt.xticks([0,1,2], ['aic_best_mod','bic_best_mod','mse_best_mod'])
+        plt.title('MSE pred, train_ratio = 0.75')
+        plt.show()
     
     return err_aic,err_bic,err_mse 
     
-def get_predictions2(train_ratio,z,p,aic_min_order,bic_min_order,mse_min_order,plot = False):
+def get_predictions2(train_ratio,z_,p_,aic_min_order,bic_min_order,mse_min_order,plot = False):
     err_aic = []
     err_bic = []
     err_mse = []
@@ -85,17 +101,17 @@ def get_predictions2(train_ratio,z,p,aic_min_order,bic_min_order,mse_min_order,p
     
         print('MODEL AIC')
         (p,q) = aic_min_order[i]
-        e = p_with_fit_of_z(p=p,q=q,z_true = z ,p_true =p, train_ratio=train_ratio[i], signif = True)
+        e = p_with_fit_of_z(p=p,q=q,z_true = z_ ,p_true =p_, train_ratio=train_ratio[i], signif = True)
         err_aic.append(e)
     
         print('MODEL BIC')
         (p,q) = bic_min_order[i]
-        e = p_with_fit_of_z(p=p,q=q,z_true = z ,p_true =p, train_ratio=train_ratio[i], signif = True)
+        e = p_with_fit_of_z(p=p,q=q,z_true = z_ ,p_true =p_, train_ratio=train_ratio[i], signif = True)
         err_bic.append(e)
     
         print('MODEL MSE')
         (p,q) = mse_min_order[i]
-        e = p_with_fit_of_z(p=p,q=q,z_true = z ,p_true =p, train_ratio=train_ratio[i], signif = True)
+        e = p_with_fit_of_z(p=p,q=q,z_true = z_ ,p_true =p_, train_ratio=train_ratio[i], signif = True)
         err_mse.append(e)
     
     if plot == True:
